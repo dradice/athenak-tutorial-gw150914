9ec71879eebffb0d775b266c23692373e8f00c8d
David Radice <dur566@psu.edu>
2025-05-29 17:49:17 +0200

    Changed default options for super_rest.py

--- scripts/cce/super_rest.py
+++ scripts/cce/super_rest.py
@@ -2,10 +2,10 @@ import scri
 from scri.SpEC.file_io import write_to_h5
 import numpy as np
 
-# time after junk radiation
-t0 = 1000
-# roughly 2 initial orbital periods
-w0 = 800
+# t0 - w0 should be after junk radiation
+t0 = 200
+# roughly 2 gravitational wave cycles 
+w0 = 150
 # interpolate to these times
 t_interp = np.linspace(0, 2500, 16384)
 
